e0136e673eba547692572b0fbd140fed8b0715d3
hpTati <hp@LAPTOP-EPLNTLPO>
2021-02-26 20:10:04 -0400

    practica 2 tercera parte

--- Practica2/Practica2/Program.cs
+++ Practica2/Practica2/Program.cs
@@ -6,6 +6,7 @@ namespace Practica2
     {
         static void Main(string[] args)
         {
+
             //1.Que pida un número y diga si es primo o no.
             Console.WriteLine("ingrese un numero entero");
             int numeroPrimo = Convert.ToInt32(Console.ReadLine());
@@ -47,19 +48,72 @@ namespace Practica2
             }
 
             //6.Que imprima la suma de todos los números que van del 1 al 100.
+            int suma = 0;
+            for (int numero = 1; numero <= 100; numero++)
+            {
+                suma = numero * (numero + 1) / 2;
+            }
+            Console.WriteLine("La suma es: {0}", suma);
 
 
             //7.Que imprima la suma de todos los números pares que van del 1 al 100.
+            int sumaPares = 0;
+            for (int numero = 2; numero <= 100; numero= numero +2)
+            {
+                sumaPares = sumaPares + numero;
+            }
+            Console.WriteLine("La suma de numeros pares es: {0}", sumaPares);
 
 
             //8.Que imprima la suma de todos los números impares que van del 1 al 100.
+            int sumaImpar= 0;
+            for (int numero = 1; numero <= 100; numero=numero + 2)
+            {
+                sumaImpar = sumaImpar + numero;
+            }
+            Console.WriteLine("La suma de numeros impartes es: {0}", sumaImpar);
 
 
             //9.Que imprima la suma de todos los números pares que van del 1 al 100 y diga cuántos hay.
+            int sumPares = 0;
+            int cantidad = 0;
+            for (int numero = 2; numero <= 100; numero = numero + 2)
+            {
+                sumPares = sumPares + numero;
+                cantidad++;
+            }
+            Console.WriteLine("La suma de {0} numeros pares es: {1}", cantidad, sumPares);
+
 
             //10.Que imprima la suma de todos los números impares que van del 1 al 100 y diga cuántos hay.
+            int sumImpar = 0;
+            int cantidad1 = 0;
+            for (int numero = 1; numero <= 100; numero = numero + 2)
+            {
+                sumImpar = sumImpar + numero;
+                cantidad1 ++;
+            }
+            Console.WriteLine("La suma de {0} numeros impares es: {1}", cantidad1, sumImpar);
 
             //11.Que pida dos números y muestre todos los números que van desde el primero al segundo.Se debe controlar que los valores son correctos.
+            Console.WriteLine("Digite el primero numero");
+            int primerNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo numero");
+            int segundoNum = Convert.ToInt32(Console.ReadLine());
+
+            if (primerNum >= segundoNum)
+            {
+                Console.WriteLine("el primer numero es menor al segundo");
+            }
+            else
+            {
+                for (int index = primerNum; index <= segundoNum; index++)
+                {
+                    Console.WriteLine(primerNum);
+                    primerNum++;
+                }
+            }
 
             //12.Que pida dos números y muestre todos los números pares que van desde el primero al segundo. Se debe controlar que los valores son correctos.
 
@@ -72,6 +126,7 @@ namespace Practica2
             //16.Que pida un número y muestre en pantalla el mismo número de asteriscos.
 
             //17.Que muestre los números del 1 al 100 en una tabla de 10x10.
+
             //18.Que escriba las tablas de multiplicar del 0 al 10.
 
             //19.Que muestre la tabla de multiplicar de un número cualquiera.
