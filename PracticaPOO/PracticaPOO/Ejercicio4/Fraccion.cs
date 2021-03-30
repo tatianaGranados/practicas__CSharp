26ed33b871716c7f7e70e08fbd79785197f7cbaa
hpTati <hp@LAPTOP-EPLNTLPO>
2021-03-29 23:45:48 -0400

    practica v3

---
+++ PracticaPOO/PracticaPOO/Ejercicio4/Fraccion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    public class Fraccion
+    {
+        private int numerador;
+        private int denominador;
+        
+
+    }
+}
--- PracticaPOO/PracticaPOO/Ejercicio4/Program.cs
+++ PracticaPOO/PracticaPOO/Ejercicio4/Program.cs
@@ -10,6 +10,35 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
+
+            //Fraccion fraccion1 = new Fraccion();
+            //Fraccion fraccion2 = new Fraccion();
+
+            while (true)
+            {
+                Console.WriteLine("Operador de fracciones: ¿Que desea realizar?");
+
+                Console.WriteLine("1. sumar");
+                Console.WriteLine("2. Restar");
+                Console.WriteLine("3. Multiplicar");
+                Console.WriteLine("4. Dividir");
+                int select = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("introduzca fraccion 1");
+                string fraccion1 = Console.ReadLine();
+                string fraccion2 = Console.ReadLine();
+
+                int num = fraccion1[0];
+                int den = fraccion1[2];
+
+                switch (select)
+                {
+                    case 1:
+
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
